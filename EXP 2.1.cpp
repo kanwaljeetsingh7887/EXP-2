c3b81ef8a1826dab3df094d9cd7973774213b1d3
Kanwaljeet Singh <97405821+kanwaljeetsingh7887@users.noreply.github.com>
2024-08-01 23:29:35 +0530

    Add files via upload

---
+++ EXP 2.1.cpp
@@ -0,0 +1,25 @@
+//KANWALJEET SINGH
+//ENTC B2
+//EXP 2.1
+//23070123124
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "size of char: " << sizeof(char) << " byte" << endl;
+    cout << "size of int: " << sizeof(int) << " byte" << endl;
+    cout << "size of float: " << sizeof(float) << " byte" << endl;
+    cout << "size of double: " << sizeof(double) << " byte" << endl;
+    cout << "size of short int: " << sizeof(short int) << "byte" << endl;
+    cout << "size of long int: " << sizeof(long int) << "byte" << endl;
+    cout << "size of unsigned char: " << sizeof(unsigned char) << "byte" << endl;
+    cout << "size of signed char: " << sizeof(signed char) << "byte" << endl;
+    cout << "size of unsigned int: " << sizeof(unsigned int) << "byte" << endl;
+    cout << "size of signed int: " << sizeof(signed int) << "byte" << endl;
+    cout << "size of unsigned long int: " << sizeof(unsigned long int) << "byte" << endl;
+    cout << "size of signed long int: " << sizeof(signed long int) << "byte" << endl;
+    cout << "size of unsigned short int: " << sizeof(unsigned short int) << "byte" << endl;
+    cout << "size of signed short int: " << sizeof(signed short int) << "byte" << endl;
+    cout << "size of unsigned long long int: " << sizeof(unsigned long long int) << "byte" << endl;
+    cout << "size of bool: " << sizeof(bool) << "byte" << endl;
+    return 0;
+}
